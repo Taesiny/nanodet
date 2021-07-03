bc2e5ec5326658c04b65e79d10bcb09824fb85b2
Taesiny <32998825+Taesiny@users.noreply.github.com>
2021-07-03 15:05:47 +0200

    Update pan.py

--- nanodet/model/fpn/pan.py
+++ nanodet/model/fpn/pan.py
@@ -60,7 +60,7 @@ class PAN(FPN):
         used_backbone_levels = len(laterals)
         for i in range(used_backbone_levels - 1, 0, -1):
             laterals[i - 1] += F.interpolate(
-                laterals[i], scale_factor=2, mode='bilinear')
+                laterals[i], scale_factor=2, mode='bilinear', align_corners=True)
 
         # build outputs
         # part 1: from original levels
@@ -70,7 +70,7 @@ class PAN(FPN):
 
         # part 2: add bottom-up path
         for i in range(0, used_backbone_levels - 1):
-            inter_outs[i + 1] += F.interpolate(inter_outs[i], scale_factor=0.5, mode='bilinear')
+            inter_outs[i + 1] += F.interpolate(inter_outs[i], scale_factor=0.5, mode='bilinear', align_corners=True)
 
         outs = []
         outs.append(inter_outs[0])
