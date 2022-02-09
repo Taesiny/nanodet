26596f353040c23d1dbdc17b859316613e08efe5
Taesiny <32998825+Taesiny@users.noreply.github.com>
2022-02-09 05:42:37 +0100

    Update convnext.py

--- nanodet/model/backbone/convnext.py
+++ nanodet/model/backbone/convnext.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import timm
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 
